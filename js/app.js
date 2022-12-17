850f3d458f6c78123cb56167f3193441e8f95379
Travis <travishersh0@gmail.com>
2022-12-17 16:55:11 -0500

    Square can now only be clicked once

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+const playerTurn = document.querySelector('#player-turn')
+const winStrategy = document.querySelector('#win-strategy')
 const resetButton = document.querySelector('#reset-button')
-const playerTurn = document.querySelector('player-turn')
 const tttBoard = document.querySelector('#ttt-board')
 
 const spaces = ''
@@ -16,31 +17,69 @@ const makeBoard = () => {
         const playArea = document.createElement('div')
         playArea.classList.add('playArea')
         tttBoard.appendChild(playArea)
-        playArea.addEventListener('click', areaClicked)
+        playArea.addEventListener('click', playAreaClicked)
+        console.log(playArea.classList)
     }
 }
 
-const areaClicked = (event) => {
-    console.log(`testing`)
-    const areaID = event.target.id
-    console.log(event)
-    if (!spaces[areaID]) {
-        spaces[areaID] = currentPlayer
+const playAreaClicked = (event) => {
+    const areaStyle = event.target.innerText
+    if (!areaStyle) {
         event.target.innerText = currentPlayer
-
+        if (playerWon()) {
+            playerTurn.innerText = `${currentPlayer} has won!`;
+            // restart();
+            return;
+        }
         currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne
     }
 }
 
-function handleClick() {
+const playerWon = () => {
+    if (spaces[0] === currentPlayer) {
+        if (spaces[1] && spaces[2] === currentPlayer) {
+            winStrategy.innerText = `${currentPlayer} wins up top!`
+            return true
+        }else if (spaces[3] && spaces[6] === currentPlayer) {
+            winStrategy.innerText = `${currentPlayer} wins on the left side!`
+            return true
+        }else if (spaces[4] && spaces[8] === currentPlayer) {
+            winStrategy.innerText = `${currentPlayer} wins diagonally!`
+            return true
+        }
+    }
+    if (spaces[8] === currentPlayer) {
+        if (spaces[2] && spaces[5] === currentPlayer) {
+          strategy.innerText = `${currentPlayer} wins on the right side!`;
+          return true;
+        }else if (spaces[6] && spaces[7] === currentPlayer) {
+          strategy.innerText = `${currentPlayer} wins on the bottom`;
+          return true;
+        }
+    }
+    if (spaces[4] === currentPlayer) {
+        if (spaces[1] && spaces[7] === currentPlayer) {
+          strategy.innerText = `${currentPlayer} wins vertically on middle`;
+          return true;
+        }else if (spaces[3] && spaces[5] === currentPlayer) {
+          strategy.innerText = `${currentPlayer} wins horizontally on the middle`;
+          return true;
+        }else if (spaces[2] && spaces[6] === currentPlayer) {
+          strategy.innerText = `${currentPlayer} wins diagonally`;
+          return true;
+        }
+    }
+}
+
+function changeButton() {
     resetButton.textContent = 'Restart Game!'
 }
 
 resetButton.addEventListener('click', () => {
-    handleClick()
+    changeButton()
     makeBoard()
 })
 
 // document.addEventListener('DOMContentLoaded', () => {
-//     makeBoard()
+//     playAreaClicked()
 // })
