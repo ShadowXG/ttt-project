3315135fac79ca35e016adeba57cb0058a65ce5f
Travis <travishersh0@gmail.com>
2022-12-17 12:41:50 -0500

    Made an Area clicked function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 const resetButton = document.querySelector('#reset-button')
+const playerTurn = document.querySelector('player-turn')
 const tttBoard = document.querySelector('#ttt-board')
 
-const startGame = () => {
-    console.log(`testing`)
-}
+const spaces = []
+const playerOne = 'X'
+const playerTwo = 'O'
+let currentPlayer = playerOne
 
 const makeBoard = () => {
     for (let i = 1; i <= 9; i++) {
         const area = document.createElement('div')
         area.classList.add('area')
         tttBoard.appendChild(area)
+        area.addEventListener('click', areaClicked)
+    }
+}
+
+const areaClicked = (event) => {
+    console.log(`testing`)
+    const areaID = event.target.id
+    console.log(event)
+    if (!spaces[areaID]) {
+        console.log(spaces[areaID])
+        spaces[areaID] = currentPlayer
+        event.target.innerText = currentPlayer
     }
 }
 
@@ -17,10 +31,7 @@ function handleClick() {
     resetButton.textContent = 'Restart Game!'
 }
 
-resetButton.addEventListener('click', () => {
-    startGame()
-    handleClick()
-})
+resetButton.addEventListener('click', handleClick)
 
 document.addEventListener('DOMContentLoaded', () => {
     makeBoard()
