7aaebc85c517a6a691b1a42fa018f8f8d9ec59a4
Travis <travishersh0@gmail.com>
2022-12-17 11:41:12 -0500

    Added a button that changes from Start Game to Reset Game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+const resetButton = document.querySelector('#reset-button')
 const tttBoard = document.querySelector('#ttt-board')
+
+const startGame = () => {
+    console.log(`testing`)
+}
 
 const makeBoard = () => {
     for (let i = 1; i <= 9; i++) {
@@ -7,6 +12,15 @@ const makeBoard = () => {
         tttBoard.appendChild(area)
     }
 }
+
+function handleClick() {
+    resetButton.textContent = 'Restart Game!'
+}
+
+resetButton.addEventListener('click', () => {
+    startGame()
+    handleClick()
+})
 
 document.addEventListener('DOMContentLoaded', () => {
     makeBoard()
