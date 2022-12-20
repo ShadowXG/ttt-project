e45861562182fe954dc776befe2b97d64ee910c4
Travis <travishersh0@gmail.com>
2022-12-19 19:10:53 -0500

    Added a reset board function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-const playerTurn = document.querySelector('#player-turn')
+const Turn = document.querySelector('#player-turn')
 const winStrategy = document.querySelector('#win-strategy')
 const resetButton = document.querySelector('#reset-button')
 const tttBoard = document.querySelector('#ttt-board')
 
-const array = []
+let array = []
 const playerOne = 'X'
 const playerTwo = 'O'
 let currentPlayer = playerOne
+console.log(currentPlayer)
 
+const playerTurn = () => {
+    console.log(currentPlayer)
+    Turn.innerText = `It's your turn! Player: ${currentPlayer}`
+}
 const makeBoard = () => {
     while (tttBoard.firstChild) {
         tttBoard.removeChild(tttBoard.firstChild)
@@ -19,7 +24,6 @@ const makeBoard = () => {
         tttBoard.appendChild(playArea)
         playArea.addEventListener('click', playAreaClicked)
         playArea.id = `${i}`
-        console.log(playArea.classList)
     }
 }
 
@@ -30,57 +34,73 @@ const playAreaClicked = (event) => {
         console.log(array[arrayId])
         event.target.innerText = currentPlayer
         if (checkForWinner()) {
-            playerTurn.innerText = `${currentPlayer} has won!`;
-            // restart();
+            Turn.innerText = `${currentPlayer} has won!`;
+            resetBoard()
             return;
         }
         currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne
+        playerTurn()
     }
 }
 
 const checkForWinner = () => {
     if (array[1] === currentPlayer) {
-        if (array[2] && array[3] === currentPlayer) {
+        if (array[2] === currentPlayer && array[3] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins up to top`;
           return true;
-        }else if (array[4] && array[7] === currentPlayer) {
+        }else if (array[4] === currentPlayer && array[7] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins on the left`;
           return true;
+        }else if (array[5] === currentPlayer && array[9] === currentPlayer) {
+            winStrategy.innerText = `${currentPlayer} wins diagonally`;
+            return true;
         }
       }else if (array[9] === currentPlayer) {
-        if (array[3] && array[6] === currentPlayer) {
+        if (array[3] === currentPlayer && array[6] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins on the right`;
           return true;
-        }else if (array[7] && array[8] === currentPlayer) {
+        }else if (array[7] === currentPlayer && array[8] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins on the bottom`;
           return true;
         }
       }else if (array[5] === currentPlayer) {
-        if (array[2] && array[8] === currentPlayer) {
+        if (array[2] === currentPlayer && array[8] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins vertically on middle`;
           return true;
-        }else if (array[3] && array[6] === currentPlayer) {
+        }else if (array[3] === currentPlayer && array[6] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins horizontally on the middle`;
           return true;
-        }else if (array[3] && array[7] === currentPlayer) {
+        }else if (array[3] === currentPlayer && array[7] === currentPlayer) {
           winStrategy.innerText = `${currentPlayer} wins diagonally`;
           return true;
-        }else if (array[1] && array[9] === currentPlayer) {
-            winStrategy.innerText = `${currentPlayer} wins diagonally`;
-            return true;
         }
     }
 }
 
-function changeButton() {
-    resetButton.textContent = 'Restart Game!'
+const resetBoard = (event) => {
+    console.log(`resetBoard has triggered`)
+    // array.forEach((array, i) => {
+    for (let i = 1; i <= 9; i++) {
+        console.log(`Still something in there`)
+        array.pop()
+    }
+    console.log(array)
+    setTimeout(() => {
+        winStrategy.innerText = ''
+        playerTurn()
+    }, 3000)
+    makeBoard()
+}
+
+const changeButton = () => {
+    resetButton.textContent = 'New Game!'
 }
 
 resetButton.addEventListener('click', () => {
     changeButton()
-    makeBoard()
+    resetBoard()
 })
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     playAreaClicked()
-// })
+document.addEventListener('DOMContentLoaded', () => {
+    playerTurn()
+})
